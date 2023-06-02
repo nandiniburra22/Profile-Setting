b2df3d065574fe4a28c2180cc5d4d599a145a675
Nandini Goud <nandiniburra22@gmail.com>
2023-06-02 22:19:09 +0530

    First Commit

--- app/src/main/java/com/example/profilesetting/MainActivity.kt
+++ app/src/main/java/com/example/profilesetting/MainActivity.kt
@@ -3,30 +3,54 @@ package com.example.profilesetting
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class ProfileSettingsActivity : AppCompatActivity() {
+class Profilesetting : AppCompatActivity() {
 
-    private lateinit var etUsername: EditText
-    private lateinit var btnSave: Button
+    private lateinit var etFullName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPhoneNumber: EditText
+    private lateinit var spinnerGender: Spinner
+    private lateinit var etAge: EditText
+    private lateinit var etAbout: EditText
+    private lateinit var etInterest: EditText
+    private lateinit var etAddress: EditText
+    private lateinit var btnSaveChanges: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_settings)
+        setContentView(R.layout.activity_profilesetting)
 
-        etUsername = findViewById(R.id.etUsername)
-        btnSave = findViewById(R.id.btnSave)
+        etFullName = findViewById(R.id.etFullName)
+        etEmail = findViewById(R.id.etEmail)
+        etPhoneNumber = findViewById(R.id.etPhoneNumber)
+        spinnerGender = findViewById(R.id.spinnerGender)
+        etAge = findViewById(R.id.etAge)
+        etAbout = findViewById(R.id.etAbout)
+        etInterest = findViewById(R.id.etInterest)
+        etAddress = findViewById(R.id.etAddress)
+        btnSaveChanges = findViewById(R.id.btnSaveChanges)
 
-        btnSave.setOnClickListener {
-            saveProfileSettings()
+        btnSaveChanges.setOnClickListener {
+            saveChanges()
         }
     }
 
-    private fun saveProfileSettings() {
-        val username = etUsername.text.toString()
-        // Save the username to your app's backend or preferences
+    private fun saveChanges() {
+        val fullName = etFullName.text.toString()
+        val email = etEmail.text.toString()
+        val phoneNumber = etPhoneNumber.text.toString()
+        val gender = spinnerGender.selectedItem.toString()
+        val age = etAge.text.toString()
+        val about = etAbout.text.toString()
+        val interest = etInterest.text.toString()
+        val address = etAddress.text.toString()
 
-        Toast.makeText(this, "Profile settings saved", Toast.LENGTH_SHORT).show()
+        // Save the profile data to your app's backend or preferences
+
+        Toast.makeText(this, "Changes saved", Toast.LENGTH_SHORT).show()
     }
 }
+
---
+++ app/src/main/res/layout/activity_edit_profile/xml.kt
@@ -0,0 +1,4 @@
+package layout.activity_edit_profile
+
+class xml {
+}
