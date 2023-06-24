89a6eebcb0d105357a5febeb24e2a9388b95acb0
Nandini Goud <nandiniburra22@gmail.com>
2023-06-24 18:05:01 +0530

    First Commit

--- app/src/main/java/com/example/profilesetting/MainActivity.kt
+++ app/src/main/java/com/example/profilesetting/MainActivity.kt
@@ -1,52 +1,54 @@
 package com.example.profilesetting
 
+import android.widget.*
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.menu.MaterialMenuItem
-import com.google.android.material.menu.MaterialPopupMenu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit_profile1)
 
-        // TODO: Add your code here
-        // You can access the views by their respective IDs and perform any necessary operations
-        val btnSaveChanges = findViewById<Button>(R.id.btnSaveChanges)
-        btnSaveChanges.setOnClickListener {
-            // Perform action on button click
-            // For example, you can retrieve the text from EditText fields
-            val fullName = findViewById<EditText>(R.id.etFullName).text.toString()
-            val email = findViewById<EditText>(R.id.etEmail).text.toString()
-            val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber).text.toString()
-// Inside your activity or fragment code
-            val genderMenu = findViewById<MaterialPopupMenu>(R.id.genderMenu)
-            val maleMenuItem = findViewById<MaterialMenuItem>(R.id.maleMenuItem)
-            val femaleMenuItem = findViewById<MaterialMenuItem>(R.id.femaleMenuItem)
+        // Access the views by their respective IDs
+        val fullNameEditText = findViewById<EditText>(R.id.etFullName)
+        val emailEditText = findViewById<EditText>(R.id.etEmail)
+        val phoneNumberEditText = findViewById<EditText>(R.id.etPhoneNumber)
+        val genderSpinner = findViewById<Spinner>(R.id.GenderSpinner)
+        val aboutEditText = findViewById<EditText>(R.id.etAbout)
+        val interestEditText = findViewById<EditText>(R.id.etInterest)
+        val addressEditText = findViewById<EditText>(R.id.etAddress)
+        val SaveChangesEditText = findViewById<EditText>(R.id.etSaveChanges)
 
-// Set click listeners for menu items
-            maleMenuItem.setOnMenuItemClickListener { menuItem ->
-                // Handle male menu item click
-                true // Return true if you have consumed the event
+        // Create an ArrayAdapter for the gender spinner
+        val genderOptions = arrayOf("Male", "Female", "Other")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        genderSpinner.adapter = adapter
+        genderSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedGender = genderOptions[position]
+                // Do something with the selected gender
             }
 
-            femaleMenuItem.setOnMenuItemClickListener { menuItem ->
-                // Handle female menu item click
-                true // Return true if you have consumed the event
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle case when nothing is selected
             }
+        }
 
-// Show the popup menu when needed
-            genderMenu.show()
+        // Set a click listener for the "Save Changes" button
+        SaveChangesEditText.setOnClickListener {
+            // Retrieve the values from the EditText fields
+            val fullName = fullNameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val phoneNumber = phoneNumberEditText.text.toString()
+            val selectedGender = genderSpinner.selectedItem.toString()
+            val about = aboutEditText.text.toString()
+            val interest = interestEditText.text.toString()
+            val address = addressEditText.text.toString()
 
-            val about = findViewById<EditText>(R.id.etAbout).text.toString()
-            val interest = findViewById<EditText>(R.id.etInterest).text.toString()
-            val address = findViewById<EditText>(R.id.etAddress).text.toString()
-
-            // You can then use the retrieved values as needed
-            // For instance, you can save them to a database or display them in a Toast message
-            // Example Toast message:
-            val message = "Full Name: $fullName\nEmail: $email\nPhone Number: $phoneNumber\nAbout: $about\nInterest: $interest\nAddress: $address"
+            // Use the retrieved values as needed
+            val message = "Full Name: $fullName\nEmail: $email\nPhone Number: $phoneNumber\nGender: $selectedGender\nAbout: $about\nInterest: $interest\nAddress: $address"
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
     }
